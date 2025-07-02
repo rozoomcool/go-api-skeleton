daeea74cdd8c697cb824ebb0f3e825df43debe83
rozoomcool <rozoom.um@gmail.com>
2025-07-03 02:44:41 +0300

    change user tabele

--- internal/db/migrations/20250702215142_new_user_table.sql
+++ internal/db/migrations/20250702215142_new_user_table.sql
@@ -2,11 +2,7 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 CREATE TABLE IF NOT EXISTS users (
- id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
- email VARCHAR(255) NOT NULL UNIQUE,
- password VARCHAR(255) NOT NULL,
- created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+ guid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 );
 -- +goose StatementEnd
 
