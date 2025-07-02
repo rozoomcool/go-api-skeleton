c422a72fd0254e118e1cd3bc8f68d84d6886ff5b
rozoomcool <rozoom.um@gmail.com>
2025-07-02 23:39:11 +0300

    load configs in main file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"rozoomcool/go-api-skeleton/internal/config"
+)
 
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to load configs: %v", err))
+	}
+
+	fmt.Println(cfg.DBPassword)
 }
