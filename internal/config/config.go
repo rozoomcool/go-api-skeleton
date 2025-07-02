e4f78c8cb3a7b9d2bd6af26440b8a8b87968360c
rozoomcool <rozoom.um@gmail.com>
2025-07-03 00:40:58 +0300

    set up new value in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,6 +12,7 @@ type Config struct {
 	Port       int           `mapstructure:"PORT"`
 	DBHost     string        `mapstructure:"DB_HOST"`
 	DBPort     int           `mapstructure:"DB_PORT"`
+	DBUser     string        `mapstructure:"DB_USER"`
 	DBPassword string        `mapstructure:"DB_PASSWORD"`
 	DBName     string        `mapstructure:"DB_NAME"`
 	JWTSecret  string        `mapstructure:"JWT_SECRET"`
